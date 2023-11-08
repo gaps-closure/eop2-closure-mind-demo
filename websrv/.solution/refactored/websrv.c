ec9e93467f7ca84c867064ea542246e464a739d3
Michael Kaplan <mkaplan@peratonlabs.com>
2023-11-08 15:55:34 +0000

    refactor for conflict analysis

--- websrv/.solution/refactored/websrv.c
+++ websrv/.solution/refactored/websrv.c
@@ -170,6 +170,8 @@ int handle_get_metadata(struct mg_connection *c, struct mg_http_message *hm) {
   return 0;
 }
 
+
+
 #pragma cle begin FUN_WSEND_VIDEO
 int wsend_video(void *arg) {
 #pragma cle end FUN_WSEND_VIDEO
@@ -188,6 +190,10 @@ int wsend_video(void *arg) {
     }
   }
   return 0;
+}
+
+int wsend_video_wrapper(void *arg) {
+  return wsend_video(arg);
 }
 
 void webfn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
@@ -223,7 +229,7 @@ void run_webserver() {
   fprintf(stderr, "Starting HTTPS camera API endpoint on %s/api\n", s_https_addr);
   fprintf(stderr, "Starting WSS video endpoint on %s/ws/video\n", s_listen_on);
   mg_http_listen(&mgr, s_https_addr, webfn, (void *) 1);
-  mg_timer_add(&mgr, FRAME_INTERVAL, MG_TIMER_REPEAT, wsend_video, &mgr);
+  mg_timer_add(&mgr, FRAME_INTERVAL, MG_TIMER_REPEAT, wsend_video_wrapper, &mgr);
   for (;;) mg_mgr_poll(&mgr, POLL_INTERVAL);
   mg_mgr_free(&mgr);
 }
